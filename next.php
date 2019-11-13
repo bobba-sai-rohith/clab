53345d66ff724dd71d8dae7d3857ceb445d47d3b
Born TO CODE <bobbasairohith@gmail.com>
2019-11-13 18:43:06 +0530

    Update next.php

--- next.php
+++ next.php
@@ -1,1 +1,11 @@
 
+<?php
+
+
+echo ' Name' . $_GET['na'].'';
+echo ' date  '. $_GET['date'].'';
+echo ' age '. $_GET['age'].'';
+echo ' from address'. $_GET['frm'].'';
+echo ' to address'. $_GET['to'].'';
+
+?>
